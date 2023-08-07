deef21221fb47b34f93812a3cddeb61caa327523
Violeta <holasoyviole@gmail.com>
2023-08-07 10:40:51 -0300

    comentarios de los components

--- ToDoList/src/components/List.jsx
+++ ToDoList/src/components/List.jsx
@@ -1,32 +1,48 @@
-import { useSelector, useDispatch } from 'react-redux';
-import { deleteTodo, toggleTodo } from '../redux/actions';
+import { useSelector, useDispatch } from "react-redux";
+import { deleteTodo, toggleTodo } from "../redux/actions";
 
 const List = () => {
-    const todos = useSelector((state) => state.todo);
-    const dispatch = useDispatch();
+  //Obtener el estado 'todo' del store de Redux.
+  const todos = useSelector((state) => state.todo);
+  const dispatch = useDispatch();
 
-    const handleDelete = (id) => {
-        dispatch(deleteTodo(id));
-    };
+  //Funcion para manejar la eliminación de tareas, utiliza el ID para pasarle a la accion del deleteTodo mediante el dispatch.
+  const handleDelete = (id) => {
+    dispatch(deleteTodo(id));
+  };
 
-    const handleToggle = (id) => {
-        dispatch(toggleTodo(id));
-    };
+  //Función para manejar el evento de cambiar a complete una tarea, tambien utiliza el ID para pasarle al toggleTodo.
+  const handleToggle = (id) => {
+    dispatch(toggleTodo(id));
+  };
 
-    if(!todos || todos.length === 0) {
-        return <div>No todos found</div>
-    }
+  //Si en la lista no hay tareas o está vacía. Retorna un mensaje que dice que no se encontraron tareas.
+  if (!todos || todos.length === 0) {
+    return <div>No todos found</div>;
+  }
   return (
     <ul>
-        {todos?.map((todo) => (
-            <li key={todo.id}>
-                <input type='checkbox' checked={todo.completed} onChange={() => handleToggle(todo.id)} />
-                <span style={{textDecoration: todo.completed ? 'line-through' : 'none'}}>{todo.task}</span>
-                <button onClick={() => handleDelete(todo.id)}>Delete</button>
-            </li>
-        ))}
+      {/* Acá itera sobre la lista de tareas utilizando un 'map' y renderiza cada tarea en un <li> */}
+      {todos?.map((todo) => (
+        <li key={todo.id}>
+          {/* Renderiza un checkbox para marcar el estado de completado de la tarea. Llama a 'handleToggle' cuando cambia */}
+          <input
+            type="checkbox"
+            checked={todo.completed}
+            onChange={() => handleToggle(todo.id)}
+          />
+          {/* Renderiza el nombre de la tarea. Si está completada, se agrega la decoración de 'line-through' */}
+          <span
+            style={{ textDecoration: todo.completed ? "line-through" : "none" }}
+          >
+            {todo.task}
+          </span>
+          {/* Renderiza un botón 'Delete' para eliminar la tarea. Llama a 'handleDelete' cuando se hace clic */}
+          <button onClick={() => handleDelete(todo.id)}>Delete</button>
+        </li>
+      ))}
     </ul>
-  )
-}
+  );
+};
 
 export default List;
--- ToDoList/src/components/Reminder.jsx
+++ ToDoList/src/components/Reminder.jsx
@@ -4,29 +4,35 @@ import { setTodo } from "../redux/actions";
 import PropTypes from 'prop-types';
 
 const Reminder = ({ todoId }) => {
+  //Defino el estado local todo con valor inicial vacío.
   const [todo, setTodoState] = useState("");
   const dispatch = useDispatch();
 
+  //manejo de envio del formulario, previniendo el error y si el campo está vacío, no hace nada. 
   const handleSubmit = (e) => {
     e.preventDefault();
     if (todo.trim() === "") return;
 
+    //Llama a la acción 'setTodo' el todoId y el contenido del campo de la tarea, luego setea el campo y lo vacía.
     dispatch(setTodo(todoId, todo));
     setTodoState("");
   };
 
   return (
     <form onSubmit={handleSubmit}>
+      {/* El input controlado muestra el contenido del campo de tarea y actualiza el estado 'todo' en cada cambio */}
       <input
         type="text"
         value={todo}
         onChange={(e) => setTodoState(e.target.value)}
       />
+       {/* El botón 'Set Todo' envía el formulario cuando se hace clic, lo que dispara 'handleSubmit' */}
       <button type="submit">Set Todo</button>
     </form>
   );
 };
 
+// Define la prop 'todoId' como un string requerido
 Reminder.propTypes = {
   todoId: PropTypes.string.isRequired, 
 };
